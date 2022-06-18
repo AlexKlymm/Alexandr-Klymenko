5041a668946eb1daaa2923107ef715dc51107d00
AlexKlymm <warebarian5@gmail.com>
2022-06-18 12:56:42 +0200

    exan2

--- homeworks/homeworks40/exam/assets/js/dist/main.dev.js
+++ homeworks/homeworks40/exam/assets/js/dist/main.dev.js
@@ -13,9 +13,48 @@ $(function () {
     dots: true,
     infinite: true,
     slidesToShow: 3,
-    slidesToScroll: 3
+    slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [{
+      breakpoint: 1430,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+        dots: true
+      }
+    }, {
+      breakpoint: 1000,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1 //   dots: false
+
+      }
+    }, {
+      breakpoint: 1000,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false
+      }
+    }, {
+      breakpoint: 555,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: false
+      }
+    }]
   });
-});
+}); // $(document).ready(function(){
+//     $('.hamburger').click(function(event){
+//         $('.header, .hamburger, .mobile_menu').toggleClass('active');
+//         $('body').toggleClass('lock')
+// });
+// });
+
 var whoWriteArticle = [{
   writer: "Alja Brun",
   articleBirthday: "20 Jan 2020",
@@ -41,84 +80,84 @@ var whoWriteArticle = [{
   writer: "Ella Ward",
   articleBirthday: "30 Nov 2019",
   titleArticle: "Example: 4",
-  articleText: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  articleText: "Lorem ipsum dolor sit amet consectetur adipisicing elit,amet consectetur adipisicing elit.",
   articleImg: "assets/photos/IndustrialForNews3.png",
   writerImg: "assets/photos/person3.png"
 }, {
   writer: "Allina Ward",
   articleBirthday: "30 Nov 2019",
   titleArticle: "Example: 5",
-  articleText: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  articleText: "Lorem ipsum dolor sit amet consectetur adipisicing elit,amet consectetur adipisicing elit.",
   articleImg: "assets/photos/IndustrialForNews3.png",
   writerImg: "assets/photos/person3.png"
 }, {
   writer: "Tanya Ward",
   articleBirthday: "30 Nov 2019",
   titleArticle: "Example: 6",
-  articleText: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  articleText: "Lorem ipsum dolor sit amet consectetur adipisicing elit,amet consectetur adipisicing elit.",
   articleImg: "assets/photos/IndustrialForNews3.png",
   writerImg: "assets/photos/person3.png"
 }, {
   writer: "Diana Ward",
   articleBirthday: "30 Nov 2019",
   titleArticle: "Example: 7",
-  articleText: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  articleText: "Lorem ipsum dolor sit amet consectetur adipisicing elit,amet consectetur adipisicing elit.",
   articleImg: "assets/photos/IndustrialForNews3.png",
   writerImg: "assets/photos/person3.png"
 }, {
   writer: "Zinaida Ward",
   articleBirthday: "30 Nov 2019",
   titleArticle: "Example: 8",
-  articleText: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  articleText: "Lorem ipsum dolor sit amet consectetur adipisicing elit,amet consectetur adipisicing elit.",
   articleImg: "assets/photos/IndustrialForNews3.png",
   writerImg: "assets/photos/person3.png"
 }, {
   writer: "Katya Ward",
   articleBirthday: "30 Nov 2019",
   titleArticle: "Example: 9",
-  articleText: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  articleText: "Lorem ipsum dolor sit amet consectetur adipisicing elit,amet consectetur adipisicing elit.",
   articleImg: "assets/photos/IndustrialForNews3.png",
   writerImg: "assets/photos/person3.png"
 }, {
   writer: "Aljona Ward",
   articleBirthday: "30 Nov 2019",
   titleArticle: "Example: 10",
-  articleText: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  articleText: "Lorem ipsum dolor sit amet consectetur adipisicing elit,amet consectetur adipisicing elit.",
   articleImg: "assets/photos/IndustrialForNews3.png",
   writerImg: "assets/photos/person3.png"
 }, {
   writer: "Albina Ward",
   articleBirthday: "30 Nov 2019",
   titleArticle: "Example: 11",
-  articleText: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  articleText: "Lorem ipsum dolor sit amet consectetur adipisicing elit,amet consectetur adipisicing elit.",
   articleImg: "assets/photos/IndustrialForNews3.png",
   writerImg: "assets/photos/person3.png"
 }, {
   writer: "Aktaviana Ward",
   articleBirthday: "30 Nov 2019",
   titleArticle: "Example: 12",
-  articleText: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  articleText: "Lorem ipsum dolor sit amet consectetur adipisicing elit,amet consectetur adipisicing elit.",
   articleImg: "assets/photos/IndustrialForNews3.png",
   writerImg: "assets/photos/person3.png"
 }, {
   writer: "Viktorya Ward",
   articleBirthday: "30 Nov 2019",
   titleArticle: "Example: 13",
-  articleText: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  articleText: "Lorem ipsum dolor sit amet consectetur adipisicing elit,amet consectetur adipisicing elit.",
   articleImg: "assets/photos/IndustrialForNews3.png",
   writerImg: "assets/photos/person3.png"
 }, {
   writer: "Sara Ward",
   articleBirthday: "30 Nov 2019",
   titleArticle: "Example: 14",
-  articleText: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  articleText: "Lorem ipsum dolor sit amet consectetur adipisicing elit,amet consectetur adipisicing elit.",
   articleImg: "assets/photos/IndustrialForNews3.png",
   writerImg: "assets/photos/person3.png"
 }, {
   writer: "Kira Ward",
   articleBirthday: "30 Nov 2019",
   titleArticle: "Example: 15",
-  articleText: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  articleText: "Lorem ipsum dolor sit amet consectetur adipisicing elit,amet consectetur adipisicing elit.",
   articleImg: "assets/photos/IndustrialForNews3.png",
   writerImg: "assets/photos/person3.png"
 }];
@@ -126,7 +165,7 @@ var whoWriteArticle = [{
 function rez() {
   var html = '';
   whoWriteArticle.forEach(function (article) {
-    html += "\n            <div class=\"sliderItem\">\n                <img src=\"".concat(article.articleImg, "\" alt=\"\">\n                <div class=\"articleWrapper\">\n                    <div class=\"textWrapper\">\n                        <h5>").concat(article.titleArticle, "</h5>\n                        <p>").concat(article.articleText, "</p>\n                    </div>\n                    <div class=\"writerWrapper\">\n                        <img src=\"").concat(article.writerImg, "\" alt=\"\">\n                        <div>\n                            <p>").concat(article.writer, "</p>\n                            <span>").concat(article.articleBirthday, "</span>\n                        </div>\n                    </div>\n                </div>\n            </div>\n        ");
+    html += "\n            <div class=\"sliderItem\">\n                <img src=\"".concat(article.articleImg, "\" alt=\"\" class=\"articleImg\">\n                <div class=\"articleWrapper\">\n                    <div class=\"textWrapper\">\n                        <h5>").concat(article.titleArticle, "</h5>\n                        <p>").concat(article.articleText, "</p>\n                    </div>\n                    <div class=\"writerWrapper\">\n                        <img src=\"").concat(article.writerImg, "\" alt=\"\">\n                        <div>\n                            <p>").concat(article.writer, "</p>\n                            <span>").concat(article.articleBirthday, "</span>\n                        </div>\n                    </div>\n                </div>\n            </div>\n        ");
   });
   document.getElementById('newsSlider1').innerHTML = html;
 }
@@ -135,3 +174,30 @@ function rez() {
 rez();
 var gallery = document.getElementById('gallery');
 lightGallery(gallery);
+var map = L.map('map').setView([49.4470822958475, 7.899966114385027], 13);
+L.tileLayer('	https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+L.marker([49.4470822958475, 7.899966114385027]).addTo(map).bindPopup('A pretty CSS3 popup.<br> Easily customizable.').openPopup();
+$('a[href^="#"').on('click', function () {
+  var href = $(this).attr('href');
+  $('html, body').animate({
+    scrollTop: $(href).offset().top
+  });
+  return false;
+});
+$(document).ready(function () {
+  $('.hamburger').click(function (event) {
+    $('.header, .hamburger, .mobileMenu').toggleClass('active');
+    $('body').toggleClass('lock');
+  });
+});
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(document).scrollTop() > 800) {
+      $("header").addClass("darkBCG");
+    } else {
+      $("header").removeClass("darkBCG");
+    }
+  });
+});
--- homeworks/homeworks40/exam/assets/js/main.js
+++ homeworks/homeworks40/exam/assets/js/main.js
@@ -7,16 +7,60 @@ $(function(){
         touchMove: true,
 
     });
-    
     $('.newsSlider').slick({
         arrows: true,
         dots: true,
         infinite: true,
         slidesToShow: 3,
-        slidesToScroll: 3
-    })
+        slidesToScroll: 3,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        responsive: [
+          {
+            breakpoint: 1430,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 1000,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            //   dots: false
+            }
+          },
+          {
+            breakpoint: 1000,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              arrows: false
+            }
+          },
+          {
+            breakpoint: 555,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              arrows: false,
+              dots: false
+            }
+          }
+        ]
+      });
+
 });
 
+// $(document).ready(function(){
+//     $('.hamburger').click(function(event){
+//         $('.header, .hamburger, .mobile_menu').toggleClass('active');
+//         $('body').toggleClass('lock')
+// });
+// });
 
 const whoWriteArticle = [
     {
@@ -47,7 +91,7 @@ const whoWriteArticle = [
         writer: "Ella Ward",
         articleBirthday: "30 Nov 2019",
         titleArticle: "Example: 4",
-        articleText: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        articleText: "Lorem ipsum dolor sit amet consectetur adipisicing elit,amet consectetur adipisicing elit.",
         articleImg: "assets/photos/IndustrialForNews3.png",
         writerImg: "assets/photos/person3.png"
     },
@@ -55,7 +99,7 @@ const whoWriteArticle = [
         writer: "Allina Ward",
         articleBirthday: "30 Nov 2019",
         titleArticle: "Example: 5",
-        articleText: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        articleText: "Lorem ipsum dolor sit amet consectetur adipisicing elit,amet consectetur adipisicing elit.",
         articleImg: "assets/photos/IndustrialForNews3.png",
         writerImg: "assets/photos/person3.png"
     },
@@ -63,7 +107,7 @@ const whoWriteArticle = [
         writer: "Tanya Ward",
         articleBirthday: "30 Nov 2019",
         titleArticle: "Example: 6",
-        articleText: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        articleText: "Lorem ipsum dolor sit amet consectetur adipisicing elit,amet consectetur adipisicing elit.",
         articleImg: "assets/photos/IndustrialForNews3.png",
         writerImg: "assets/photos/person3.png"
     },
@@ -71,7 +115,7 @@ const whoWriteArticle = [
         writer: "Diana Ward",
         articleBirthday: "30 Nov 2019",
         titleArticle: "Example: 7",
-        articleText: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        articleText: "Lorem ipsum dolor sit amet consectetur adipisicing elit,amet consectetur adipisicing elit.",
         articleImg: "assets/photos/IndustrialForNews3.png",
         writerImg: "assets/photos/person3.png"
     },
@@ -79,7 +123,7 @@ const whoWriteArticle = [
         writer: "Zinaida Ward",
         articleBirthday: "30 Nov 2019",
         titleArticle: "Example: 8",
-        articleText: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        articleText: "Lorem ipsum dolor sit amet consectetur adipisicing elit,amet consectetur adipisicing elit.",
         articleImg: "assets/photos/IndustrialForNews3.png",
         writerImg: "assets/photos/person3.png"
     },
@@ -87,7 +131,7 @@ const whoWriteArticle = [
         writer: "Katya Ward",
         articleBirthday: "30 Nov 2019",
         titleArticle: "Example: 9",
-        articleText: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        articleText: "Lorem ipsum dolor sit amet consectetur adipisicing elit,amet consectetur adipisicing elit.",
         articleImg: "assets/photos/IndustrialForNews3.png",
         writerImg: "assets/photos/person3.png"
     },
@@ -95,7 +139,7 @@ const whoWriteArticle = [
         writer: "Aljona Ward",
         articleBirthday: "30 Nov 2019",
         titleArticle: "Example: 10",
-        articleText: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        articleText: "Lorem ipsum dolor sit amet consectetur adipisicing elit,amet consectetur adipisicing elit.",
         articleImg: "assets/photos/IndustrialForNews3.png",
         writerImg: "assets/photos/person3.png"
     },
@@ -103,7 +147,7 @@ const whoWriteArticle = [
         writer: "Albina Ward",
         articleBirthday: "30 Nov 2019",
         titleArticle: "Example: 11",
-        articleText: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        articleText: "Lorem ipsum dolor sit amet consectetur adipisicing elit,amet consectetur adipisicing elit.",
         articleImg: "assets/photos/IndustrialForNews3.png",
         writerImg: "assets/photos/person3.png"
     },
@@ -111,7 +155,7 @@ const whoWriteArticle = [
         writer: "Aktaviana Ward",
         articleBirthday: "30 Nov 2019",
         titleArticle: "Example: 12",
-        articleText: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        articleText: "Lorem ipsum dolor sit amet consectetur adipisicing elit,amet consectetur adipisicing elit.",
         articleImg: "assets/photos/IndustrialForNews3.png",
         writerImg: "assets/photos/person3.png"
     },
@@ -119,7 +163,7 @@ const whoWriteArticle = [
         writer: "Viktorya Ward",
         articleBirthday: "30 Nov 2019",
         titleArticle: "Example: 13",
-        articleText: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        articleText: "Lorem ipsum dolor sit amet consectetur adipisicing elit,amet consectetur adipisicing elit.",
         articleImg: "assets/photos/IndustrialForNews3.png",
         writerImg: "assets/photos/person3.png"
     },
@@ -127,7 +171,7 @@ const whoWriteArticle = [
         writer: "Sara Ward",
         articleBirthday: "30 Nov 2019",
         titleArticle: "Example: 14",
-        articleText: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        articleText: "Lorem ipsum dolor sit amet consectetur adipisicing elit,amet consectetur adipisicing elit.",
         articleImg: "assets/photos/IndustrialForNews3.png",
         writerImg: "assets/photos/person3.png"
     },
@@ -135,7 +179,7 @@ const whoWriteArticle = [
         writer: "Kira Ward",
         articleBirthday: "30 Nov 2019",
         titleArticle: "Example: 15",
-        articleText: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        articleText: "Lorem ipsum dolor sit amet consectetur adipisicing elit,amet consectetur adipisicing elit.",
         articleImg: "assets/photos/IndustrialForNews3.png",
         writerImg: "assets/photos/person3.png"
     }
@@ -146,7 +190,7 @@ function rez() {
     whoWriteArticle.forEach(article => {
         html+= `
             <div class="sliderItem">
-                <img src="${article.articleImg}" alt="">
+                <img src="${article.articleImg}" alt="" class="articleImg">
                 <div class="articleWrapper">
                     <div class="textWrapper">
                         <h5>${article.titleArticle}</h5>
@@ -169,3 +213,39 @@ rez();
 
 let gallery = document.getElementById('gallery');
 lightGallery(gallery);
+
+const map = L.map('map').setView([49.4470822958475, 7.899966114385027], 13);
+
+L.tileLayer('	https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([49.4470822958475, 7.899966114385027]).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
+
+
+$('a[href^="#"').on('click', function() {
+    let href = $(this).attr('href');
+    $('html, body').animate({
+        scrollTop: $(href).offset().top
+    });
+    return false;
+});
+
+$(document).ready(function(){
+    $('.hamburger').click(function(event){
+        $('.header, .hamburger, .mobileMenu').toggleClass('active');
+        $('body').toggleClass('lock')
+    });
+});
+
+$(document).ready(function() {
+    $(window).scroll(function() {
+      if ($(document).scrollTop() > 800) {
+        $("header").addClass("darkBCG");
+      } else {
+        $("header").removeClass("darkBCG");
+      }
+    });
+});
