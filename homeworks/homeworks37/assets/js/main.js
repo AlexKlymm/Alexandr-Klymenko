5f8636242647f7d0994b3be9285f941d4bc3cb0c
AlexKlymm <warebarian5@gmail.com>
2022-07-04 18:34:56 +0200

    hw 37

---
+++ homeworks/homeworks37/assets/js/main.js
@@ -0,0 +1,17 @@
+// функция подгрузки страницы с ajax
+function loadPage(page) {
+    $.get('pages/'+page+'.html', (html) => {
+        $("#page__content").html(html);
+    })
+};
+
+loadPage('main');
+
+$(function() {
+    $('.nav-masthead a').on('click', function(e) {
+        e.preventDefault();
+        loadPage($(this).attr("href"));
+        $('.nav-link.active').removeClass("active");
+        $(this).addClass("active");
+    })
+}); 
